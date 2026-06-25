c71d01e9d61d49df9296f7016095fb75bbf6aa20
Wesley Ruiz <wruizm@est.utn.ac.cr>
2026-06-25 16:41:26 -0600

    Se sube la clase disciplina actualizada

--- src/disciplinas/Disciplina.java
+++ src/disciplinas/Disciplina.java
@@ -3,17 +3,59 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package disciplinas;
+import java.time.LocalDate;
 import personas.Persona;
 /**
  *
  * @author emalv
  */
-public class Disciplina {
+public class Disciplina extends Persona{
+    
     private TipoDisciplina tipo;
+    private String entrenador;
+    private enumCategoria categoria;
+
+    public Disciplina(int identificacion, String nombreCompleto, LocalDate fechaNacimiento, int telefonoContacto) {
+        super(identificacion, nombreCompleto, fechaNacimiento, telefonoContacto);
+        
+    }
 
     public TipoDisciplina getTipo() {
         return tipo;
     }
+
+    public String getEntrenador() {
+        return entrenador;
+    }
+
+    public enumCategoria getCategoria() {
+        return categoria;
+    }
+
+    public void setTipo(TipoDisciplina tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setEntrenador(String entrenador) {
+        this.entrenador = entrenador;
+    }
+
+    public void setCategoria(enumCategoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Disciplina(TipoDisciplina tipo, String entrenador, enumCategoria categoria, int identificacion, String nombreCompleto, LocalDate fechaNacimiento, int telefonoContacto) {
+        super(identificacion, nombreCompleto, fechaNacimiento, telefonoContacto);
+        this.tipo = TipoDisciplina.Futbol;
+        this.entrenador = entrenador;
+        this.categoria = enumCategoria.JUVENIL;
+    }
+
+    @Override
+    public String toString() {
+        return "Disciplina: " +  tipo + "\nEntrenador:" + entrenador + "\nCategoria:" + categoria;
+    }
+    
     
     
     
