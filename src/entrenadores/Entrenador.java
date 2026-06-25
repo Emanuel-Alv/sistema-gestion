6df9ddc27dd5e774aad04bc41e949212ccef9b14
estarli-var <lucordoba@est.utn.ac.cr>
2026-06-25 16:19:48 -0600

    se volvio a subir de nuevo
    la clase estba vacia cuando se subio asi qe se volvio a subir ya con las cosas

--- src/entrenadores/Entrenador.java
+++ src/entrenadores/Entrenador.java
@@ -3,11 +3,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package entrenadores;
-
+import personas.Persona;
+import disciplinas.TipoDisciplina;
+import java.time.LocalDate;
 /**
  *
  * @author Student
  */
-public class Entrenador {
+public class Entrenador extends Persona{
+     private TipoDisciplina especialidad; 
+     private int experiencia;
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+ 
     
+   public TipoDisciplina getEspecialidad(){
+    return this.especialidad;
+   } 
+
+   public void seEspecialidad(TipoDisciplina especialidad) {
+        this.especialidad = especialidad;
+    }
+   
+   public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+    
+    public Entrenador(int experiencia, TipoDisciplina especialidad, int identificacion, String nombreCompleto, LocalDate fechaNacimiento, int telefonoContacto) {
+        super(identificacion, nombreCompleto, fechaNacimiento, telefonoContacto);
+        this.especialidad = especialidad;
+        this.experiencia = experiencia;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return super.toString() + 
+               " Especialidad: " + this.especialidad.getTipo() + 
+               " Experiencia: " + this.experiencia + " años";
+    }
 }
